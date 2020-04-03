742ee399546ec8daa7c4b77f806b75959c086195
daepoid <daepoid@gmail.com>
2020-04-03 14:08:40 +0900

    업로드

---
+++ NoDGV/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace NoDGV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button_remove = new System.Windows.Forms.Button();
+            this.button_edit = new System.Windows.Forms.Button();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_load = new System.Windows.Forms.Button();
+            this.button_save = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(500, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.label3);
+            this.panel5.Controls.Add(this.label2);
+            this.panel5.Controls.Add(this.label1);
+            this.panel5.Controls.Add(this.panel8);
+            this.panel5.Controls.Add(this.panel7);
+            this.panel5.Controls.Add(this.panel6);
+            this.panel5.Location = new System.Drawing.Point(0, 0);
+            this.panel5.MinimumSize = new System.Drawing.Size(150, 75);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(300, 150);
+            this.panel5.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "위치";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "메뉴";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "가게 이름";
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.textBox3);
+            this.panel8.Location = new System.Drawing.Point(90, 100);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(210, 50);
+            this.panel8.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(0, 15);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 21);
+            this.textBox3.TabIndex = 0;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.textBox2);
+            this.panel7.Location = new System.Drawing.Point(90, 50);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(210, 50);
+            this.panel7.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(0, 15);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 21);
+            this.textBox2.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.textBox1);
+            this.panel6.Location = new System.Drawing.Point(90, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(210, 50);
+            this.panel6.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button_remove);
+            this.panel4.Controls.Add(this.button_edit);
+            this.panel4.Controls.Add(this.button_add);
+            this.panel4.Controls.Add(this.button_load);
+            this.panel4.Controls.Add(this.button_save);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 150);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(300, 300);
+            this.panel4.TabIndex = 0;
+            // 
+            // button_remove
+            // 
+            this.button_remove.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_remove.Location = new System.Drawing.Point(75, 242);
+            this.button_remove.Name = "button_remove";
+            this.button_remove.Size = new System.Drawing.Size(150, 48);
+            this.button_remove.TabIndex = 5;
+            this.button_remove.Text = "삭제";
+            this.button_remove.UseVisualStyleBackColor = true;
+            this.button_remove.Click += new System.EventHandler(this.button_remove_Click);
+            // 
+            // button_edit
+            // 
+            this.button_edit.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_edit.Location = new System.Drawing.Point(75, 184);
+            this.button_edit.Name = "button_edit";
+            this.button_edit.Size = new System.Drawing.Size(150, 48);
+            this.button_edit.TabIndex = 4;
+            this.button_edit.Text = "수정";
+            this.button_edit.UseVisualStyleBackColor = true;
+            this.button_edit.Click += new System.EventHandler(this.button_edit_Click);
+            // 
+            // button_add
+            // 
+            this.button_add.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_add.Location = new System.Drawing.Point(75, 126);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(150, 48);
+            this.button_add.TabIndex = 3;
+            this.button_add.Text = "추가";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_load
+            // 
+            this.button_load.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_load.Location = new System.Drawing.Point(75, 10);
+            this.button_load.Name = "button_load";
+            this.button_load.Size = new System.Drawing.Size(150, 48);
+            this.button_load.TabIndex = 2;
+            this.button_load.Text = "불러오기";
+            this.button_load.UseVisualStyleBackColor = true;
+            this.button_load.Click += new System.EventHandler(this.button_load_Click);
+            // 
+            // button_save
+            // 
+            this.button_save.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_save.Location = new System.Drawing.Point(75, 68);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(150, 48);
+            this.button_save.TabIndex = 1;
+            this.button_save.Text = "저장";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tableLayoutPanel1);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(500, 450);
+            this.panel2.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 100);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(500, 350);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.MinimumSize = new System.Drawing.Size(500, 100);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(500, 100);
+            this.panel3.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("굴림", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(130, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(240, 50);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "맛집 정보";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_remove;
+        private System.Windows.Forms.Button button_edit;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_load;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ NoDGV/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NoDGV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            initialize_TLP(true);
+            Text = "맛집 정보";
+        }
+        
+        const string path = "../data.txt";
+        int checked_Count = 0;
+        List<Restaurant> restaurants = new List<Restaurant>();
+        List<CheckBox> checkBoxes = new List<CheckBox>();
+
+        Restaurant temp = new Restaurant("", "", "");
+
+        public class Restaurant
+        {
+            public string st_name { get; set; }
+            public string st_menu { get; set; }
+            public string st_location { get; set; }
+
+            public Restaurant(string _st_name, string _st_menu, string _st_location)
+            {
+                st_name = _st_name;
+                st_menu = _st_menu;
+                st_location = _st_location;
+            }
+
+            //public override bool Equals(object obj)
+            //{
+            //    return base.Equals(obj);
+            //}
+
+            //public override int GetHashCode()
+            //{
+            //    return base.GetHashCode();
+            //}
+
+            public static bool operator == (Restaurant r1, Restaurant r2)
+            {
+                if(r1.st_name.Equals(r2.st_name) && r1.st_menu.Equals(r2.st_menu) && r1.st_location.Equals(r2.st_location))
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            public static bool operator != (Restaurant r1, Restaurant r2)
+            {
+                if (r1.st_name.Equals(r2.st_name) && r1.st_menu.Equals(r2.st_menu) && r1.st_location.Equals(r2.st_location))
+                {
+                    return false;
+                }
+                return true;
+            }
+
+        }
+
+        private void set_LabelFont(Label label, string str, int font, bool bold)
+        {
+            label.Text = str;
+            label.AutoSize = true;
+            label.Size = new Size(130, 20);
+            label.Font = new Font(label.Font.FontFamily, font, bold ? FontStyle.Bold : FontStyle.Regular);
+        }
+
+        private void clear_TextBoxs()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void clear_CheckBoxs()
+        {
+            for(int i = 0; i < checkBoxes.Count; i++)
+            {
+                checkBoxes[i].Checked = false;
+            }
+        }
+
+        private bool check_DataOverlapping(Restaurant new_restaurant)
+        {
+
+            int index = restaurants.FindIndex(x => x == new_restaurant);
+            if(index != -1)
+            {
+                string temp = "가게 이름 : " + new_restaurant.st_name + "\n메뉴 : " + new_restaurant.st_menu + "\n주소 : " + new_restaurant.st_location;
+                MessageBox.Show(temp + "\n데이터가 중복입니다.");
+                return true;
+            }
+            return false;
+            //for (int i = 0; i < restaurants.Count; i++)
+            //{
+            //    if (restaurants[i] == new_restaurant)
+            //    {
+            //        string temp = "가게 이름 : " + new_restaurant.st_name + "\n메뉴 : " + new_restaurant.st_menu + "\n주소 : " + new_restaurant.st_location;
+            //        MessageBox.Show(temp + "\n데이터가 중복입니다.");
+            //        return true;
+            //    }
+            //}
+            //return false;
+        }
+
+        private void add_Data_TLP(string st_name, string st_menu, string st_location)
+        {
+            Restaurant new_restaurant = new Restaurant(st_name, st_menu, st_location);
+            if (!check_DataOverlapping(new_restaurant))
+            {
+                restaurants.Add(new_restaurant);
+                add_Control_TLP(new_restaurant);
+                clear_TextBoxs();
+                clear_CheckBoxs();
+            }                      
+        }
+
+        private void add_Control_TLP(Restaurant restaurant)
+        {
+            CheckBox checkbox = new CheckBox();
+            checkbox.AutoSize = false;
+            checkbox.Size = new Size(20, 15);
+            checkBoxes.Add(checkbox);
+            checkbox.CheckedChanged += delegate (object obj, EventArgs arg)
+            {
+                if (checkbox.Checked)
+                {
+                    checked_Count++;
+                    textBox1.Text = restaurant.st_name;
+                    textBox2.Text = restaurant.st_menu;
+                    textBox3.Text = restaurant.st_location;
+
+                    temp.st_name = restaurant.st_name;
+                    temp.st_menu = restaurant.st_menu;
+                    temp.st_location = restaurant.st_location;
+                }
+                else
+                {
+                    if(checked_Count == 1)
+                    {
+                        clear_TextBoxs();
+                    }
+                    checked_Count--;
+                }
+            };
+            tableLayoutPanel1.Controls.Add(checkbox);
+
+            Label label1 = new Label();
+            set_LabelFont(label1, restaurant.st_name, 13, false);
+            tableLayoutPanel1.Controls.Add(label1);
+
+            Label label2 = new Label();
+            set_LabelFont(label2, restaurant.st_menu, 13, false);
+            tableLayoutPanel1.Controls.Add(label2);
+
+            Label label3 = new Label();
+            set_LabelFont(label3, restaurant.st_location, 13, false);
+            tableLayoutPanel1.Controls.Add(label3);
+        }
+
+        private void initialize_TLP(bool restaurants_clear_bit)
+        {
+            if(restaurants_clear_bit)
+            {
+                restaurants.Clear();
+            }            
+            checkBoxes.Clear();
+            tableLayoutPanel1.Controls.Clear();
+
+            Panel panel = new Panel();
+            tableLayoutPanel1.Controls.Add(panel);
+
+            Label label1 = new Label();
+            set_LabelFont(label1, "가게 이름", 16, true);
+            tableLayoutPanel1.Controls.Add(label1);
+
+            Label label2 = new Label();
+            set_LabelFont(label2, "메뉴", 16, true);
+            tableLayoutPanel1.Controls.Add(label2);
+
+            Label label3 = new Label();
+            set_LabelFont(label3, "위치", 16, true);
+            tableLayoutPanel1.Controls.Add(label3);
+        }
+
+        private void button_load_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                initialize_TLP(true);
+                string[] lines = File.ReadAllLines(path, Encoding.Default);
+                foreach (string line in lines)
+                {
+                    string[] parseLine = line.Split('|');
+                    add_Data_TLP(parseLine[0], parseLine[1], parseLine[2]);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("File NOT Found.");
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("ERROR - IOException");
+            }
+        }
+
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] lines = new string[restaurants.Count];
+                for(int i = 0; i < restaurants.Count; i++)
+                {
+                    lines[i] = restaurants[i].st_name + '|' + restaurants[i].st_menu + '|' + restaurants[i].st_location;
+                }
+                File.WriteAllLines(path, lines, Encoding.Default);
+                MessageBox.Show("저장 완료");
+            }
+            catch(IOException)
+            {
+                MessageBox.Show("ERROR - IOException");
+            }
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (restaurants.Count == 0)
+                {
+                    initialize_TLP(true);
+                }
+                if (textBox1.Text.Equals("") || textBox2.Text.Equals("") || textBox3.Text.Equals(""))
+                {
+                    MessageBox.Show("비어있는 요소가 있습니다.");
+                    return;
+                }
+                add_Data_TLP(textBox1.Text, textBox2.Text, textBox3.Text);
+                MessageBox.Show("Add Complete");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Add Error");
+            }
+            
+        }
+
+        private void button_edit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = checkBoxes.Count - 1; i >= 0; i--)
+                {
+                    if (checkBoxes[i].Checked && restaurants[i] == temp)
+                    {
+                        restaurants[i].st_name = textBox1.Text;
+                        restaurants[i].st_menu = textBox2.Text;
+                        restaurants[i].st_location = textBox3.Text;
+                        break;
+                    }
+                }
+                initialize_TLP(false);
+                for (int i = 0; i < restaurants.Count; i++)
+                {
+                    add_Control_TLP(restaurants[i]);
+                }
+                clear_TextBoxs();
+                checked_Count = 0;
+                MessageBox.Show("Edit Complete");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Edit Error");
+            }            
+        }
+
+        private void button_remove_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = checkBoxes.Count - 1; i >= 0; i--)
+                {
+                    if (checkBoxes[i].Checked)
+                    {
+                        checkBoxes.RemoveAt(i);
+                        restaurants.RemoveAt(i);
+                        for (int j = 0; j < tableLayoutPanel1.ColumnCount; j++)
+                        {
+                            tableLayoutPanel1.Controls.RemoveAt((i + 1) * 4);
+                        }
+                    }
+                }
+                clear_TextBoxs();
+                checked_Count = 0;
+
+                MessageBox.Show("Remove Complete");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Remove Error");
+            }
+            
+        }
+    }
+}
